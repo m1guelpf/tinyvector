e4ee89f02e6bbdb42b6358e7a84fff198246f322
Neel Redkar <neel.redkar@gmail.com>
2023-07-05 16:06:56 -0700

    add API_KEY to server

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,11 @@ mod similarity;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+	let api_key : &str = option_env!("API_KEY").unwrap_or("");
+	if api_key == "" {
+		println!("Running in open mode, no API_KEY set");
+	}
+
 	tracing_subscriber::registry()
 		.with(tracing_subscriber::fmt::layer().with_filter(
 			EnvFilter::try_from_default_env().unwrap_or_else(|_| "tinyvector=info".into()),
