580e5a6b9e77a284785359557a074d7bb63b9c02
Ben Harris <byronbenharris@Bens-MacBook-Pro.local>
2023-07-05 11:46:17 -0400

    wip

---
+++ src/search.rs
@@ -0,0 +1,81 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use std::{collections::{HashMap}};
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, JsonSchema, PartialEq, Eq)]
+
+pub enum Operation {
+	#[serde(rename = "and")]
+	And,
+	#[serde(rename = "or")]
+	Or,
+    #[serde(rename = "eq")]
+	Eq,
+	#[serde(rename = "ne")]
+	Ne,
+	#[serde(rename = "gt")]
+	Gt,
+    #[serde(rename = "gte")]
+	Gte,
+    #[serde(rename = "lt")]
+	Lt,
+    #[serde(rename = "lte")]
+	Lte,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, JsonSchema, PartialEq, Eq)]
+pub struct Comparator {
+	/// Dimension of the vectors in the collection
+	pub attribute: String,
+    /// Distance metric used for querying
+	pub op: Operation,
+	/// Embeddings in the collection
+	pub value: String,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+	#[error("Filter incorrectly formatted")]
+	InvalidFilter,
+}
+
+
+
+pub fn parse(input: Option<HashMap<String, String>>) -> Option<Comparator> {
+    match input {
+		Some(input) => match input {
+			"$and" => {},
+			"$or" => {},
+			"$eq" => {},
+			"$ne" => {},
+			"$gt" => {},
+			"$gte" => {},
+			"$lt" => {},
+			"$lte" => {},
+			_ => {},
+		},
+		None => None,
+	}
+}
+
+fn get_operation_fn(op: Operation) -> impl Fn(String | bool, String | bool) -> bool {
+	match op {
+		Operation::And => and,
+		Operation::Or => or,
+		Operation::Eq => eq,
+		Operation::Ne => ne,
+        Operation::Gt => gt,
+        Operation::Gte => gte,
+        Operation::Lt => lt,
+        Operation::Lte => lte,
+    }
+}
+
+fn and(lhs: bool, rhs: bool) -> bool { lhs && rhs }
+fn or(lhs: bool, rhs: bool) -> bool { lhs || rhs }
+fn eq(lhs: String, rhs: String) -> bool { lhs == rhs }
+fn ne(lhs: String, rhs: String) -> bool { lhs != rhs }
+fn gt(lhs: String, rhs: String) -> bool { lhs > rhs }
+fn gte(lhs: String, rhs: String) -> bool { lhs >= rhs }
+fn lt(lhs: String, rhs: String) -> bool { lhs < rhs }
+fn lte(lhs: String, rhs: String) -> bool { lhs <= rhs }
